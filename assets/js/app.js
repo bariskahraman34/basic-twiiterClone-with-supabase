e2b1c5b045e098c6f10a30fc26f70bc2ff54e42c
Barış Kahraman <baris-kahra@hotmail.com>
2024-05-08 01:22:17 +0300

    Anasayfanın genel olarak tasarımı yapıldı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,19 @@ const SUPABASE_URL = 'https://cbpagjrvjcgkxqffdvsa.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNicGFnanJ2amNna3hxZmZkdnNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4OTkyNzgsImV4cCI6MjAzMDQ3NTI3OH0.otY3cxo-_MStdGztovq4RCXiXyMwYBB9tBGptWNdeJw';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-const signOut = document.querySelector('#logout');
+const signOut = document.querySelector('#log-out-btn');
+
+const textarea = document.querySelector('#textarea');
+function setNewSize() {
+   this.style.height = "1px";
+   this.style.height = this.scrollHeight + "px";
+}
+textarea.addEventListener('keyup', setNewSize);
 
 document.addEventListener('DOMContentLoaded',async () => {
     const { data, error } = await _supabase.auth.getSession();
     if(data.session === null){
-        window.location.href = "/login.html";
+        return window.location.href = "/login.html";
     }
 })
 
